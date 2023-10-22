e0e6eb887540517ec50f266afeb6b223f75c0db6
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2023-10-22 18:42:16 -0500

    haciendo actualizaciones del cliente

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -193,7 +193,7 @@ const Home = () => {
         </div>
       </div>
       <div className={style.subTitulo}>
-        <h2>SERVICIOS:RECIBIMOS</h2>
+        <h2>RECIBIMOS</h2>
       </div>
 
       <Cards servicios={servicios} />
