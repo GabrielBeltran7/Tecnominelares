1a2e2be2ed6c31bd34b14a2e126fd3444a59e9f7
AngelSilva98 <angelceesd@gmail.com>
2023-10-23 17:47:40 -0300

    arreglo carrousel

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -9,66 +9,54 @@ const contentStyle = {
   background: "#364d79",
 };
 const Carrousel = () => (
-  <Carousel autoplay effect="fade" className={style.carrousel}>
+  <Carousel effect="fade" className={style.carrousel}>
     <div className={style.image}>
       <img
-        src="https://res.cloudinary.com/dou3yyisb/image/upload/v1697084862/TecnoMateriales/logo_nvjh7f.png"
+        src="https://res.cloudinary.com/dou3yyisb/image/upload/v1697073792/TecnoMateriales/maquinaria8-ia_gcsmno.jpg"
         alt=""
-        width="100px"
       />
-      <h3>
-        TECNO<span>MINERALES</span>
-      </h3>
-      <p>Arenas & Gravas</p>
     </div>
 
     <div className={style.imageDos}>
       <img
-        src="https://res.cloudinary.com/dou3yyisb/image/upload/v1697084862/TecnoMateriales/logo_sin_fondo_qhdxvn.png"
+        src="https://res.cloudinary.com/dou3yyisb/image/upload/v1697073795/TecnoMateriales/maquinaria9-ia_ojklf4.jpg"
         alt=""
-        width="350px"
       />
     </div>
     <div className={style.imageTres}>
       <img
-        src="https://res.cloudinary.com/dou3yyisb/image/upload/v1697084862/TecnoMateriales/logo_sin_fondo_qhdxvn.png"
+        src="https://res.cloudinary.com/dou3yyisb/image/upload/v1697073796/TecnoMateriales/maquinaria7-ia_rxoygj.jpg"
         alt=""
-        width="350px"
       />
     </div>
     <div className={style.imageCuatro}>
       <img
-        src="https://res.cloudinary.com/dou3yyisb/image/upload/v1697084862/TecnoMateriales/logo_sin_fondo_qhdxvn.png"
+        src="https://res.cloudinary.com/dou3yyisb/image/upload/c_pad,b_gen_fill,w_1920,h_1080,ar_16:9,e_improve,e_sharpen/v1697748217/TecnoMateriales/WhatsApp_Image_2023-10-06_at_6.29.02_PM_dbqfs8.jpg"
         alt=""
-        width="350px"
       />
     </div>
     <div className={style.imageCinco}>
       <img
-        src="https://res.cloudinary.com/dou3yyisb/image/upload/v1697084862/TecnoMateriales/logo_sin_fondo_qhdxvn.png"
+        src="https://res.cloudinary.com/dou3yyisb/image/upload/v1697748218/TecnoMateriales/WhatsApp_Image_2023-10-06_at_6.29.03_PM_2_bisw1e.jpg"
         alt=""
-        width="350px"
       />
     </div>
     <div className={style.imageSeis}>
       <img
-        src="https://res.cloudinary.com/dou3yyisb/image/upload/v1697084862/TecnoMateriales/logo_sin_fondo_qhdxvn.png"
+        src="https://res.cloudinary.com/dou3yyisb/image/upload/v1697748223/TecnoMateriales/WhatsApp_Image_2023-10-10_at_9.34.22_PM_2_hkjulh.jpg"
         alt=""
-        width="350px"
       />
     </div>
     <div className={style.imageSiete}>
       <img
-        src="https://res.cloudinary.com/dou3yyisb/image/upload/v1697084862/TecnoMateriales/logo_sin_fondo_qhdxvn.png"
+        src="https://res.cloudinary.com/dou3yyisb/image/upload/c_pad,b_gen_fill,w_1280,h_720,ar_16:9,e_improve,e_sharpen/v1697748216/TecnoMateriales/WhatsApp_Image_2023-10-06_at_6.29.01_PM_2_dxp7cx.jpg"
         alt=""
-        width="350px"
       />
     </div>
     <div className={style.imageOcho}>
       <img
-        src="https://res.cloudinary.com/dou3yyisb/image/upload/v1697084862/TecnoMateriales/logo_sin_fondo_qhdxvn.png"
+        src="https://res.cloudinary.com/dou3yyisb/image/upload/c_pad,b_gen_fill,w_1280,h_720,ar_16:9,e_improve,e_sharpen/v1697748216/TecnoMateriales/WhatsApp_Image_2023-10-06_at_6.29.01_PM_wegtan.jpg"
         alt=""
-        width="350px"
       />
     </div>
   </Carousel>
